91d56489503e33d0ba179cb8eb151cda53e0efbe
Shawn Tsai <shawn.tsai@cdibh.com>
2022-07-22 10:55:00 +0800

    Enforce unit test

--- src/test/java/com/redhat/lab/core/domain/service/OnboardingServiceImplTest.java
+++ src/test/java/com/redhat/lab/core/domain/service/OnboardingServiceImplTest.java
@@ -1,12 +1,21 @@
 package com.redhat.lab.core.domain.service;
 
+import com.redhat.lab.core.domain.entity.CaseDo;
 import com.redhat.lab.core.domain.repository.AccountRepository;
 import com.redhat.lab.core.domain.repository.CaseRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.UUID;
+
+//import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Matchers.any;
+
+import static org.mockito.Mockito.times;
 
 class OnboardingServiceImplTest {
 
@@ -17,7 +26,20 @@ class OnboardingServiceImplTest {
 
         OnboardingService onboardingService = new OnboardingServiceImpl(caseRepository, accountRepository);
 
-        onboardingService.raiseCase("bank");
-        Mockito.verify(caseRepository).save(any());
+        CaseDo caseDo = onboardingService.raiseCase("bank");
+
+        assertThat(isUUID(caseDo.getCaseId()), is(true));
+        assertThat(caseDo.getProductKind(), equalTo("bank"));
+        assertThat(caseDo.getStatus(), equalTo(CaseDo.Status.APPLYING));
+        Mockito.verify(caseRepository, times(1)).save(caseDo);
+    }
+
+    private boolean isUUID(String uuid) {
+        try {
+            UUID.fromString(uuid);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
     }
 }
